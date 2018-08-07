802090fcd4cc5a88335d60e1111649c5ae78b6f0
Yannick Boetzel <yannick.boetzel@gmail.com>
2018-08-07 16:55:18 +0200

    Added teaching 2018.

---
+++ news/-teaching_2018HS.php
@@ -0,0 +1,31 @@
+<?php
+
+$NEWS[] = [
+    "title"       => <<<EOLHTML
+Teaching Semester 2018 HS
+EOLHTML
+    , // !!! keep this on a seperate line
+
+    // will make the news apear in red
+    "isImportant" => false,
+
+    // when it happens (for sorting)
+    "date"     => "2018-09-09 00:00",
+
+    // when will it appear on the webpage, default to 'now', so will
+    // be shown imediately
+    "dateOn"   => "2018-07-01 00:00",
+
+    // when will it disappear from webpage, default date+1w
+    "dateOff"  => "2019-02-01 00:00",
+
+    "text"     => <<<EOLHTML
+
+    <a href="http://www.physik.uzh.ch/en/teaching/PHY312/HS2018.html">PHY312 Mathematical Methods of Physics I</a>
+    <a href="https://studentservices.uzh.ch/uzh/anonym/vvz/index.html?sap-language=DE&sap-ui-language=DE#/details/2018/003/SM/50859063">(Link Vorlesungsverzeichnis UZH)</a>
+
+EOLHTML
+    ]; // !!! keep this on a seperate line
+
+
+?>
